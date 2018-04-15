3853363724a9e582a3fb2e74594a60c9f4a79a21
Auston Stewart <auston@shedbuilt.net>
2018-04-15 12:19:12 -1000

    Updating packaging for blank

--- build.sh
+++ build.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 # Apply the BLFS patch to prevent segfaults when the module is built with Python 3.6.4
-patch -Np1 -i "${SHED_PATCHDIR}/libxml2-2.9.7-python3_hack-1.patch"
+patch -Np1 -i "${SHED_PKG_PATCH_DIR}/libxml2-2.9.7-python3_hack-1.patch"
 # Fix another Python 3.6.4 / GCC 7 issue
 sed -i '/_PyVerify_fd/,+1d' python/types.c
 ./configure --prefix=/usr    \
             --disable-static \
             --with-history   \
             --with-python=/usr/bin/python3 && \
-make -j $SHED_NUMJOBS && \
-make DESTDIR="$SHED_FAKEROOT" install
+make -j $SHED_NUM_JOBS && \
+make DESTDIR="$SHED_FAKE_ROOT" install
